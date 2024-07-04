eae92c2b0885f69b9489b508350ebb17bcf2b9dc
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2024-07-04 14:15:15 +0530

    Lazy Loading

--- Week 7/routing/src/App.jsx
+++ Week 7/routing/src/App.jsx
@@ -1,23 +1,35 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import './App.css'
-import { Dashboard } from './components/Dashboard'
-import { Landing } from './components/Landing'
+// import { Dashboard } from './components/Dashboard'
+// import { Landing } from './components/Landing'
+// import Dashboard from './components/Dashboard'
+const Dashboard = React.lazy(() => import('./components/Dashboard'))
+const Landing = React.lazy(() => import('./components/Landing'))
 
 function App() {
+
 
   return (
     <div>
       <div style={{background: "red", color:"black"}}>
         Fixed bar
         </div>
-        <button onClick={() => {
+
+        {/* Wroung Way to Navigate page */}
+        {/* <button onClick={() => {
           window.location.href = "/dashboard"
         }}>Dashboard</button>
 
         <button onClick={() => {
           window.location.href = "/"
-        }}>Landing</button>
+        }}>Landing</button> */}
+
+
+        {/* Wright way to do this */}
+
+        
     <BrowserRouter>
+    <Appbar/>
       <Routes>
         <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path='/' element={<Landing/>}/>
@@ -26,6 +38,18 @@ function App() {
     </div>
 
   )
+}
+function Appbar(){
+const navigate = useNavigate();
+  return <div>
+  <button onClick={() => {
+    navigate("dashboard")
+  }}>Dashboard</button>
+  <button onClick={() => {
+    navigate('/')
+  }}>Loadong...</button>  
+  </div>
+
 }
 
 export default App
